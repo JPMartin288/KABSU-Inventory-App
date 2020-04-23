356c47747a9acc97c3f87725881e47acf9aa809f
daleyk <31221760+daleyk@users.noreply.github.com>
2020-04-22 21:03:11 -0500

    Update InsertData.sql
    fixed insertanimal and insertsample

--- Database Files/Database Insertion Files/InsertData.sql
+++ Database Files/Database Insertion Files/InsertData.sql
@@ -14,22 +14,23 @@ IN AnimalID VARCHAR(32),
 IN Name VARCHAR(128),
 IN Breed VARCHAR(64),
 IN Species VARCHAR(16),
-IN RegNum VARCHAR(32),
-IN PersonID INT)
+IN RegNum VARCHAR(32))
 BEGIN 
-INSERT kabsu.animal(AnimalID, Name, Breed, Species, RegNum, PersonID)
-VALUES(AnimalID, Name, Breed, Species, RegNum, PersonID);
+INSERT kabsu.animal(AnimalID, Name, Breed, Species, RegNum)
+VALUES(AnimalID, Name, Breed, Species, RegNum);
 END
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `InsertSample`(
-IN Valid BOOL,
-IN CanNum VARCHAR(8),
-IN AnimalID VARCHAR(32),
-IN CollDate VARCHAR(32),
+IN Valid VARCHAR(8),
+IN CanNum VARCHAR(32),
+IN Code VARCHAR(100),
+IN CollDate VARCHAR(128),
 IN NumUnits INT,
-IN PersonID INT,
-IN Notes VARCHAR(1000))
+IN Notes VARCHAR(1000),
+IN PersonName VARCHAR(100),
+IN City VARCHAR(64),
+IN State VARCHAR(2))
 BEGIN
 INSERT kabsu.sample(Valid, CanNum, AnimalID, CollDate, NumUnits, PersonID, Notes)
-VALUES(Valid, CanNum, AnimalID, CollDate, NumUnits, PersonID, Notes);
+VALUES(Valid, CanNum, Code, CollDate, NumUnits, (SELECT PersonID FROM kabsu.person WHERE Name = PersonName AND City = City AND State = State), Notes);
 END
