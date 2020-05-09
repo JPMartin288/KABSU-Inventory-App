170c0b4c6c2d619c203e8bfb0ec16fd2d7b285e8
shant haik <shant@ksu.edu>
2020-05-08 21:07:07 -0500

    Added Coded UI Testing

---
+++ WpfApp/KABSUUITests/CodedUITestFilters.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+
+
+namespace KABSUUITests
+{
+    /// <summary>
+    /// Summary description for CodedUITest1
+    /// </summary>
+    [CodedUITest]
+    public class CodedUITestFilters
+    {
+        public CodedUITestFilters()
+        {
+
+        }
+
+
+        /// <summary>
+        /// This is a test to open the "Modify Record" window
+        /// </summary>
+        [TestMethod]
+        public void CodedUITestMethod1()
+        {
+            
+            this.UIMap.OpeningModifyRecordWindow();
+        }
+        /// <summary>
+        /// This is a test to use the "Owner" filter in search
+        /// </summary>
+        [TestMethod]
+        public void CodedUITestMethod2()
+        {
+
+            this.UIMap.SearchByOwnerFilter();
+
+        }
+        /// <summary>
+        /// This is a test to use the "Breed" filter in search
+        /// </summary>
+        [TestMethod]
+        public void CodedUITestMethod3()
+        {
+
+            this.UIMap.SearchByBreedFilter();
+
+        }
+        /// <summary>
+        /// This is a test to use the "Animal Name" filter in search
+        /// We will search for "Holy Smoker" as we know it exists
+        /// </summary>
+        [TestMethod]
+        public void CodedUITestMethod4()
+        {
+
+            this.UIMap.SearchByAnimalNameFilter();
+
+        }
+        /// <summary>
+        /// This is a test to use the "Code #" filter in search
+        /// we will search for "countyo" as we know it exists 
+        /// </summary>
+        [TestMethod]
+        public void CodedUITestMethod5()
+        {
+
+            this.UIMap.SearchByCodeFilter();
+
+        }
+        /// <summary>
+        /// This is a test to use the "Can #" filter in search
+        /// we will search for "658" as we know it exists 
+        /// </summary>
+        [TestMethod]
+        public void CodedUITestMethod7()
+        {
+
+            this.UIMap.SearchByCanFilter();
+
+        }
+        /// <summary>
+        /// This is a test to use the "Town" filter in search
+        /// we will search for "Lemasa" as we know it exists 
+        /// </summary>
+        [TestMethod]
+        public void CodedUITestMethod8()
+        {
+
+            this.UIMap.SearchByTownFilter();
+
+        }
+        /// <summary>
+        /// This is a test to use the "State" filter in search
+        /// we will search for "KS" as we know it exists 
+        /// </summary>
+        [TestMethod]
+        public void CodedUITestMethod9()
+        {
+
+            this.UIMap.SearchByStateFilter();
+
+        }
+        /// <summary>
+        /// This is a test to use the "Owner" filter  AND "Breed" filter (2 filters)
+        /// we will search for "Mouse" in the owner filter and "Cross" in Breed filter as we know both exist.
+        /// </summary>
+        [TestMethod]
+        public void CodedUITestMethod10()
+        {
+
+            this.UIMap.SearchByOwnerAndCross();
+
+        }
+        /// <summary>
+        /// This is a test to use the "Owner" filter  AND "Breed" AND "Animal Name" filter (3 filters)
+        /// we will search for "Mouse" in the owner filter and "Cross" in Breed filter and "Holy Smoker" in Animal Name as we know they exist.
+        /// </summary>
+        [TestMethod]
+        public void CodedUITestMethod11()
+        {
+
+            this.UIMap.SearchByOwnerAndByBreedAndByAnimalName();
+
+        }
+        /// <summary>
+        /// This is a test to use the "Owner" filter  AND "Breed" AND "Animal Name" AND "Code" filter (4 filters)
+        /// we will search for "Mouse" in the owner filter and "Cross" in Breed filter and "Holy Smoker" in Animal Name and "54XB399" as we know they exist.
+        /// </summary>
+        [TestMethod]
+        public void CodedUITestMethod12()
+        {
+
+            this.UIMap.SearchByOwnerAndByBreedAndByAnimalNameAndByCode();
+
+        }
+
+
+        #region Additional test attributes
+
+        // You can use the following additional attributes as you write your tests:
+
+        ////Use TestInitialize to run code before running each test 
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        ////Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        #endregion
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if (this.map == null)
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        private UIMap map;
+    }
+}
---
+++ WpfApp/KABSUUITests/CodedUITestSumOfUnits.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+
+
+namespace KABSUUITests
+{
+    /// <summary>
+    /// Summary description for CodedUITest1
+    /// </summary>
+    [CodedUITest]
+    public class CodedUITestSumOfUnits
+    {
+        public CodedUITestSumOfUnits()
+        {
+        }
+        /// <summary>
+        /// This is to test the "Sum OF Units" function.
+        /// We will look for "Mouse" with the "Owner" filter, and click on "Sum Of Units" the expected result is 31
+        /// </summary>
+        [TestMethod]
+        public void CodedUITestMethod1()
+        {
+
+            this.UIMap.TestForSumOfUnits();
+
+        }
+        /// <summary>
+        /// This is to test to the total Sum Of Units without any filters or test in the search box.
+        /// The expected result is 213886
+        /// </summary>
+        [TestMethod]
+        public void CodedUITestMethod2()
+        {
+
+            this.UIMap.TestForTotalSumOfUnits();
+
+        }
+        /// <summary>
+        ///  This is to test to the total Sum Of Units with a filter ther than "Owner" in this case we will try "Breed" with the search entry "Mouse"
+        ///  The expcted result is 0
+        /// </summary>
+        public void CodedUITestMethod3()
+        {
+
+            this.UIMap.IsThisGonnaWork();
+
+        }
+ 
+
+        #region Additional test attributes
+
+        // You can use the following additional attributes as you write your tests:
+
+        ////Use TestInitialize to run code before running each test 
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        ////Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        #endregion
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if (this.map == null)
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        private UIMap map;
+    }
+}
